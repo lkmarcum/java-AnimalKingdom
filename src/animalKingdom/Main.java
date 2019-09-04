27cccb6089c5bd27b2035cf3a6db2f7a71e26032
lkmarcum <50113705+lkmarcum@users.noreply.github.com>
2019-09-04 15:14:07 -0500

    fixed semicolon error

--- src/animalKingdom/Main.java
+++ src/animalKingdom/Main.java
@@ -97,7 +97,7 @@ public class Main
 
 		System.out.println();
 
-		System.out.println("*** Mammals Sorted Alphabetically ***")
+		System.out.println("*** Mammals Sorted Alphabetically ***");
 		printAnimals(myList, a -> a instanceof Mammal);
 	}
 }
